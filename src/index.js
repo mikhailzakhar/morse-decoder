6a203f79b8c03579336766a94f3518fcf41623fb
unknown <myzakharov@yandex.ru>
2022-07-31 13:09:26 +0500

    solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let arr2 = [];
+    let arr3 = [];
+    let str = '';
+  
+    for (let i=0, j=0; i<expr.length; i+=10, j++) {
+      arr[j] = expr.slice(i, i+10);
+    }
+    //console.log(arr);
+  
+    for (let i=0; i<arr.length; i++) {
+      if (arr[i] === '**********')
+        arr2[i] = ' ';
+      else {
+        arr2[i] = '';
+        for (let j=0; j<10; j+=2) {
+          str = arr[i].slice(j, j+2);
+          //console.log(str);
+          if (str === "10") 
+            arr2[i] += '.';
+          if (str === "11") 
+            arr2[i] += '-';        
+        } 
+      }
+    }
+    //console.log(arr2);
+  
+    for (let i=0; i<arr2.length; i++) {
+      if (arr2[i] === ' ')
+        arr3[i] = ' ';
+      else 
+        for (let code in MORSE_TABLE)
+          if (code === arr2[i])
+            arr3[i] = MORSE_TABLE[code];    
+    }
+    //console.log(arr3);
+  
+    return arr3.join('');
 }
 
 module.exports = {
